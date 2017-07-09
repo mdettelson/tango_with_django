ccdd8be7eb2449f482780d48312a920f3b0f8788
mdettelson <mdettelson@gmail.com>
2017-07-09 12:55:41 -0400

    Small Formatting Changes in models.py

--- Rango/models.py
+++ Rango/models.py
@@ -5,10 +5,12 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+
     def __str__(self):
         return self.name
     def __unicode__(self):
         return self.name
+
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
