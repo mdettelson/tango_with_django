c444cb8b09959b406e435f6823b415cfa794e5e3
mdettelson <mdettelson@gmail.com>
2017-07-09 20:48:40 -0400

    Fixed name bug in admin.py, added meta class to Category model to fix naming in admin.

--- Rango/admin.py
+++ Rango/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from rango.models import Category, Page
+from Rango.models import Category, Page
 
 admin.site.register(Category)
 admin.site.register(Page)
