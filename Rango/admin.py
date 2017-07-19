27e5464afb829c4d3555e59a7d7d533ded52af8e
mdettelson <mdettelson@gmail.com>
2017-07-18 22:35:46 -0400

    made slug field auto-populate in admin interface

--- Rango/admin.py
+++ Rango/admin.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
 from Rango.models import Category, Page
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
