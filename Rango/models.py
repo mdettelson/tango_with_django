65eb055c04c8dedb0a5aed002c0b44b81c4d7677
mdettelson <mdettelson@gmail.com>
2017-07-18 22:32:58 -0400

    added tests, updated views and index template to list categories, adding slug field to category model.

--- Rango/models.py
+++ Rango/models.py
@@ -2,19 +2,28 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
+
     def __unicode__(self):
         return self.name
+
 
 class Page(models.Model):
     category = models.ForeignKey(Category)
@@ -24,5 +33,6 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+
     def __unicode__(self):
         return self.title
--- Rango/views.py
+++ Rango/views.py
@@ -1,19 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponse
+from Rango.models import Category
 
-from django.shortcuts import render
 
 def index(request):
+    # Query the database for a list of ALL categories currently stored
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our contect_dict dictionary
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+
     # Construct a dictionary to pass to the template engine as its context
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
