7472ec1bc8cf4e020add0f588613ba818c2fbb1a
mdettelson <mdettelson@gmail.com>
2017-07-19 22:56:59 -0400

    added Category creation link to index.html, added name to Rango to remove absolute links

--- TangoWithDjango/urls.py
+++ TangoWithDjango/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from Rango import views
 
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^rango/', include('Rango.urls')),
