21bcee9c0ff3a3678222e6e4175736cb72dd6d5a
mdettelson <mdettelson@gmail.com>
2017-07-19 22:59:00 -0400

    refactored max_length in forms to refer back to the models

--- Rango/forms.py
+++ Rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from Rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
+    name = forms.CharField(max_length=Category._meta.get_field('name').max_length,
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(),
                                initial=0)
@@ -18,12 +18,25 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
+    title = forms.CharField(max_length=Page._meta.get_field('title').max_length,
                             help_text="Please enter the title of the page.")
-    url = forms.URLField(max_length=200,
+    url = forms.URLField(max_length=Page._meta.get_field('url').max_length,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(),
                                initial=0)
+
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
 
 
     class Meta:
